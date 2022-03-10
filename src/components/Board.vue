781c3606f6c33967f9a179300d71781b144e5c0c
Nicolas <n.signo@hotmail.com>
2022-03-10 16:13:57 -0500

    Working board

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,84 +1,170 @@
 <template>
-    <div class="table">
-        <div class="table-cell" v-for="cell in cells" :key="cell">
-            <span>{{cell.title}}</span>
-            <div class="images">
-                <div>
-                    <img v-for="image in cell.images" :key="image" :src="image.url" :alt="image.alt">
-                </div>
-            </div>
-        </div>
+  <span>{{ loading }}</span>
+  <div class="table">
+    <div class="table-cell" v-for="cell in cells" :key="cell">
+      <span class="info-button" v-if="cell.title" v-on:click="info(cell.id)">
+        <font-awesome-icon icon="info" />
+      </span>
+      <span class="title">{{ cell.title }}</span>
+      <div class="images">
+        <img v-for="image in cell.images" :key="image" :src="image.url">
+      </div>
+      <div v-if="cell.title" :id="cell.id" class="info-card">
+        <p v-html="cell.desc"></p>
+      </div>
+      <div v-if="cell.hitsplat" class="hitsplat">
+        <img v-if="cell.damage" src="https://osrsraidsqc.com/wp-content/uploads/2022/03/Damage_hitsplat-1.png">
+        <img v-else src="https://osrsraidsqc.com/wp-content/uploads/2022/03/Tank_hitsplat.png">
+        <span class="team">Attacked by <b>{{cell.team}}</b></span>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "BoardComponent",
-        data() {
-            return {
-                cells: [
-                    {title: "Test 1",
-                        images: [
-                            {url: "https://oldschool.runescape.wiki/images/thumb/Dagannoth_Rex.png/800px-Dagannoth_Rex.png", alt: "Berseker Ring"},
-                            {url: "https://oldschool.runescape.wiki/images/Berserker_ring.png", alt: "Berseker Ring"},
-                            {url: "https://oldschool.runescape.wiki/images/Archers_ring.png", alt: "Berseker Ring"},
-                            {url: "https://oldschool.runescape.wiki/images/Warrior_ring.png", alt: "Berseker Ring"},
-                            {url: "https://oldschool.runescape.wiki/images/Seers_ring.png", alt: "Berseker Ring"},
-                        ]
-                    },
-                    {title: "Test 2"},
-                    {title: "Test 3"},
-                    {title: "Test 4"},
-                    {title: "Test 5"},
-                    {title: "Test 6"},
-                ]
-            }
-        }
+export default {
+  name: "BoardComponent",
+  methods: {
+    info: function(id) {
+      let element = document.getElementById(id);
+      if (element.style.opacity !== "1") {
+        element.style.opacity = "1";
+      }
+      else {
+        element.style.opacity = "0";
+      }
     }
+  },
+  data() {
+    return {
+      loading: false,
+      cells: []
+    }
+  },
+  async created() {
+    this.loading = true
+
+    let dataUrl = "https://opensheet.elk.sh/1TB0ZWKY80s6bQ4n40j8yKo9id-m-SzEvPbrUpHJ3fhc/1";
+    const response = await fetch(dataUrl);
+    const data = await response.json();
+    data.forEach(e => {
+      if('images' in e)
+        e.images = JSON.parse(e.images);
+      if('hitsplat' in e && e.hitsplat !== null) {
+        e.damage = e.hitsplat === "1";
+      }
+    });
+    this.cells = data;
+
+    this.loading = false;
+  }
+}
 </script>
 
 <style scoped>
-    .table {
-        color: white;
-        background-color: rgba(19, 19, 19, 0.79);
-        display: flex;
-        flex-wrap: wrap;
-        margin: 0 0 3em 0;
-        padding: 0;
-        min-width: 500px;
-    }
+.table {
+  color: white;
+  background-color: rgba(19, 19, 19, 0.79);
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-auto-flow: row ;
+  overflow: scroll;
+  margin: 0 0 3em 0;
+  padding: 0;
+  min-width: 500px;
+  width: 100%
+;
+}
 
-    .table-cell {
-        width: 33%;
-        box-sizing: border-box;
-        padding: 0.8em 1.2em;
-        overflow: hidden;
-        border: solid white;
-        display: flex;
-        flex-grow: 1;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-    }
+.table-cell {
+  box-sizing: border-box;
+  padding: 0.5rem;
+  overflow: hidden;
+  border: solid white;
+  display: flex;
+  flex-grow: 1;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  position: relative;
+  min-width: 150px;
+  height: 125px;
+}
 
-    .images {
-        display: flex;
-        flex-wrap: wrap;
-    }
-    .images div {
-        flex-grow: 1;
-        width: fit-content;
-    }
+.images {
+  max-height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-content: center;
+}
 
-    .images div img {
-        max-height: 100%;
-        max-width: 50px;
-        object-fit: cover;
-        vertical-align: bottom;
-    }
+.images img {
+  flex: 1;
+  max-height: 75%;
+  width: 100%;
+  min-width: 0;
+  padding: 0.2rem;
+  object-fit: scale-down;
+}
 
-    tr:nth-child(even) td:nth-child(odd),
-    tr:nth-child(odd) td:nth-child(even)  {
-        background-color: black;
-    }
+.info-card {
+  transition-duration: 500ms;
+  opacity: 0;
+  position: absolute;
+  background: rgba(0, 0, 0, 0.8);
+  z-index: 2;
+  height: 100%;
+  width: 100%;
+}
+
+.info-card p {
+  margin: 0.5em;
+}
+
+.title {
+  position: absolute;
+  top: 10px;
+  z-index: 1;
+}
+
+.info-button {
+  cursor: pointer;
+  background: dimgrey;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  margin: 0.5rem;
+  box-shadow: rgba(0, 0, 0, 0.35) 0 5px 15px;
+  z-index: 3;
+}
+.info-button:hover {
+  background: black;
+}
+
+.hitsplat {
+  background: rgba(0, 0, 0, 0.8);
+  position: absolute;
+  height: 100%;
+  width: 100%;
+}
+
+.hitsplat img {
+  height: inherit;
+  object-fit: scale-down;
+}
+
+.team {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  padding: 0.5rem;
+  font-size: 0.75em;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faInfo } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { VueCsvImportPlugin } from "vue-csv-import";
+
+library.add(faInfo);
+
+createApp(App)
+  .use(VueCsvImportPlugin)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount('#app')
